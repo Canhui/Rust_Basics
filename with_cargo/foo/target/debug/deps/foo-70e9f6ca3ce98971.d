e327276017e67d84fd3b86dce4d1db66fc3fe5f5
Ubuntu <azure-canhui@project-dev-6.k1gzkzvdrt4u5flknlycqxbz0h.lx.internal.cloudapp.net>
2024-01-10 07:58:52 +0000

    info

---
+++ with_cargo/foo/target/debug/deps/foo-70e9f6ca3ce98971.d
@@ -0,0 +1,5 @@
+/home/azure-canhui/Rust-Canhui-Tutorials/Rust_Basics/with_cargo/foo/target/debug/deps/foo-70e9f6ca3ce98971: src/main.rs
+
+/home/azure-canhui/Rust-Canhui-Tutorials/Rust_Basics/with_cargo/foo/target/debug/deps/foo-70e9f6ca3ce98971.d: src/main.rs
+
+src/main.rs:
